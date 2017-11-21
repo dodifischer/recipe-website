f59b4fd3a1c88fbccda210326859993057b1f180
dodifischer <33822504+dodifischer@users.noreply.github.com>
2017-11-21 12:09:08 +0100

    updated modules
    .js files

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 (function(){
-	var app = angular.module("store", []);
+	var app = angular.module("store", ['store-products']);
 
 	app.controller('StoreController', function(){
-		this.products = gems;
+		this.products = [
+			{
+				name: 'Lokum beef',
+				price: 90,
+				description: 'blazzing hot pan, olive oil, salt, done.',
+				canPurchase: true,
+				images: [
+					'images/Lokum1Thumb.jpg',
+					'images/Lokum1Full.jpg'
+					
+				],
+				reviews: [
+					{
+						stars: 5,
+						body: "Awesome",
+						author: "Nysret"
+					},
+					{
+						stars: 5,
+						body: "Best ever",
+						author: "Pep"
+					}
+				]
+			},
+			{
+				name: 'Dallas',
+				price: 120,
+				description: 'Huge steak with oils.',
+				canPurchase: true,
+				images: [
+					'images/Dallas1Thumb.jpg',
+					'images/Dallas1Full.jpg'
+				],
+				reviews: [
+					{
+						stars: 5,
+						body: "Sweeeet",
+						author: "Dodi"
+					},
+					{
+						stars: 5,
+						body: "Such good",
+						author: "Viper"
+					}
+				]
+			}
+		];
 	});
 	
 	app.controller('ReviewController', function(){
@@ -12,77 +58,5 @@
 			this.review = {};
 		};
 	});
-	
-	app.directive('productTitle', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'product-title.html'
-		};
-	});
-
-	app.directive('productPanels', function(){
-		return{
-			restrict: 'E',
-			templateUrl: 'product-panels.html',
-			controller: function(){
-				this.tab = 1;
-				this.selectTab = function(setTab) {
-					this.tab = setTab;
-				}
-				this.isSelected = function(checkTab) {
-					return this.tab === checkTab;
-				}
-			},
-			controllerAs: 'tab'
-		};
-	});
-	
-	var gems = [
-		{
-			name: 'Lokum beef',
-			price: 90,
-			description: 'blazzing hot pan, olive oil, salt, done.',
-			canPurchase: true,
-			images: [
-				'images/Lokum1Full.jpg',
-				'images/Lokum1Thumb.jpg'
-				
-			],
-			reviews: [
-				{
-					stars: 5,
-					body: "Awesome",
-					author: "Nysret"
-				},
-				{
-					stars: 5,
-					body: "Best ever",
-					author: "Pep"
-				}
-			]
-		},
-		{
-			name: 'Dallas',
-			price: 120,
-			description: 'Huge steak with oils.',
-			canPurchase: true,
-			images: [
-				'images/Dallas1Thumb.jpg',
-				'images/Dallas1Thumb.jpg'
-			],
-			reviews: [
-				{
-					stars: 5,
-					body: "Sweeeet",
-					author: "Dodi"
-				},
-				{
-					stars: 5,
-					body: "Such good",
-					author: "Viper"
-				}
-			]
-		}
-	];
 
 })();
